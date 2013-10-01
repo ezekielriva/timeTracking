77ea21aa788bd4ba5bd37a1e45fafb7c2cd6d5f1
Ezekiel <ezerivadeneiral@gmail.com>
2013-10-01 12:20:42 -0300

    Project & User scaffold. Devise included

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,8 @@ class Project < ActiveRecord::Base
   attr_accessible :name, :status_id
 
   belongs_to :status
+  has_many :projects_user
+  has_many :users, through: :projects_user
 
   validates :name, presence: true
   validates :status_id, presence: true
